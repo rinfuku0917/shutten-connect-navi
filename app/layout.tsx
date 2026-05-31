3f59cfa6d7144f9fe8178cedfee0e38bc97ad90d
hideki fukusada <hidekifukusada@hidekinoMacBook-Pro.local>
2026-05-31 11:53:03 +0900

    remove duplicate Header from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Header from '@/components/Header'
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,7 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><Header />{children}</body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
